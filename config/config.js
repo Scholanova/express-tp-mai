1dcf366226ca630bf1f2a003c8bc5359395efdd7
Cyril VUILLEMIN <cyril.vuillemin@hotmail.fr>
2020-05-28 11:46:10 +0200

    first commit mais bien

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: 'postgres',
-    password: 'moramora',
+    password: 'postgres',
     database: 'scholanova_express_mai_development',
     host: '127.0.0.1',
     dialect: 'postgres',
@@ -9,7 +9,7 @@ module.exports = {
   },
   test: {
     username: 'postgres',
-    password: 'moramora',
+    password: 'postgres',
     database: 'scholanova_express_mai_test',
     host: '127.0.0.1',
     dialect: 'postgres',
@@ -17,7 +17,7 @@ module.exports = {
   },
   production: {
     username: 'postgres',
-    password: 'moramora',
+    password: 'postgres',
     database: 'scholanova_express_mai_production',
     host: '127.0.0.1',
     dialect: 'postgres',
